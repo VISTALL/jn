e8ead51c8c97958721dd392a70dc624e175b459e
VISTALL <VISTALL@6152a259-5f98-4063-bd53-153fcf638599>
2011-10-09 17:17:46 +0000

    * фикс "exe"
    git-svn-id: https://subversion.assembla.com/svn/jn/trunk@89 6152a259-5f98-4063-bd53-153fcf638599

--- jn/develop/starter/Jn/Program.cs
+++ jn/develop/starter/Jn/Program.cs
@@ -10,48 +10,20 @@ namespace Jn
 {
     static class Program
     {
-        static String JAVA_HOME = null;
         static String XMS = "-Xms512m";
         static String XMX = "-Xmx512m";
 
         [STAThread]
         static void Main()
         {
-            load();
-            if (JAVA_HOME == null)
-            {
-                ProcessStartInfo startInfo = new ProcessStartInfo
-                                                 {
-                                                     FileName = "javaw",
-                                                     Arguments = XMS + " " + XMX + " " + " -cp ./libs/*; com.jds.jn.Jn"
-                                                 };
-                Process.Start(startInfo);
-            }
-            else
-            {
-                ProcessStartInfo startInfo = new ProcessStartInfo
-                                                 {
-                                                     FileName = JAVA_HOME + "javaw.exe",
-                                                     Arguments = XMS + " " + XMX + " " + " -cp ./libs/*; com.jds.jn.Jn"
-                                                 };
-                Process.Start(startInfo);
-            }
-    }
-       
-        static void load()
-        {
-            XmlDocument doc = new XmlDocument();
-            doc.Load("settings.xml");
+            Process p = Process.GetCurrentProcess();
 
-            XmlNode xNode = doc.GetElementsByTagName("settings").Item(0);
-
-            IConfigurationSectionHandler csh = new NameValueSectionHandler();
-            NameValueCollection nvc = (NameValueCollection)csh.Create(null, null, xNode);
-
-            JAVA_HOME = nvc.Get("JAVA_HOME");
-            XMS = nvc.Get("XMS");
-            XMX = nvc.Get("XMX");
-        }  
-
+            ProcessStartInfo startInfo = new ProcessStartInfo
+                                             {
+                                                 FileName = "javaw",
+                                                 Arguments = p.StartInfo.Arguments + " -cp ./libs/*; com.jds.jn.Jn"
+                                             };
+            Process.Start(startInfo);
+        }
     }
 }
