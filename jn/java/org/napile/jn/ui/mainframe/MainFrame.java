152a3bdb70ca78bd9d3c3de6c67b664b058e0214
VISTALL <VISTALL@6152a259-5f98-4063-bd53-153fcf638599>
2011-11-15 14:32:36 +0000

    + some prepares
    git-svn-id: https://subversion.assembla.com/svn/jn/trunk@101 6152a259-5f98-4063-bd53-153fcf638599

---
+++ jn/java/org/napile/jn/ui/mainframe/MainFrame.java
@@ -0,0 +1,29 @@
+/*
+ * Jn (Java sNiffer)
+ * Copyright (C) 2011 napile.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.napile.jn.ui.mainframe;
+
+/**
+ * @author VISTALL
+ * @date 16:40/05.11.2011
+ */
+public class MainFrame
+{
+
+}
